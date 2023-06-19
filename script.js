f6bdb7f27010083a7a35145bb518f18bb0799b0c
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-19 23:30:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+const liTag = document.getElementsByTagNames('li');
+
+for(let i=0; i<10; i++){
+	liTag[i] = function(event){
+		let getId = event.target.id;
+		if(getId === level){
+			alert("The level of the element is: ", i)
+		}
+	}
+	liTag[i]();
+	}
